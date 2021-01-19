efce4d01273e82e1a35c92d79e1c04670a2e45dd
Benjamin DiGregorio <ben@digregor.io>
2021-01-19 15:56:42 -0800

    Show restart button on game over

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,18 @@
-﻿using System.Collections;
+﻿// C# Standard Lib
+using System.Collections;
 using System.Collections.Generic;
+
+// Unity
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using TMPro;
 
 public class GameManager : MonoBehaviour {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public Button restartButton;
     public bool gameIsActive = true;
     private int score = 0;
     private float spawnRate = 1.0f;
@@ -33,5 +39,10 @@ public class GameManager : MonoBehaviour {
     public void GameOver() {
         gameIsActive = false;
         gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void RestartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
