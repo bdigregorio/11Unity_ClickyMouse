d371749d6cc2bc2722973342071d4e288001dc93
Benjamin DiGregorio <ben@digregor.io>
2021-01-19 11:37:47 -0800

    Spawn random targets every 1 sec with random force and torque applied

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+
+    private void Start() {
+        StartCoroutine(SpawnTarget());
+    }
+
+    private IEnumerator SpawnTarget() {
+        while (true) {
+            yield return new WaitForSeconds(spawnRate);
+            int randomIndex = Random.Range(0, targets.Count);
+            Instantiate(targets[randomIndex]);
+        }
+    }
+}
